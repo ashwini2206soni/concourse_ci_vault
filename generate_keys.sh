30921d578e01e32878c30a42e60f75afcb3c1665
ashwinisoni2206 <ashwini19soni@gmail.com>
2021-02-12 13:21:34 -0500

    fixed gen_key script

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-docker run --rm -v $PWD/keys/web:/keys concourse/concourse \
+docker run --rm -v "$PWD/keys/web:/keys" concourse/concourse \
 generate-key -t rsa -f /keys/session_signing_key
 
-docker run --rm -v $PWD/keys/web:/keys concourse/concourse \
+docker run --rm -v "$PWD/keys/web:/keys" concourse/concourse \
 generate-key -t ssh -f /keys/tsa_host_key
 
-docker run --rm -v $PWD/keys/worker:/keys concourse/concourse \
+docker run --rm -v "$PWD/keys/worker:/keys" concourse/concourse \
 generate-key -t ssh -f /keys/worker_key
